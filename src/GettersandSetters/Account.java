35b91da1963e7bf89c538e9c4889743a03fa7458
Ruben Montanez <ruben.a.montanez@gmail.com>
2020-02-16 11:45:13 -0600

    more exercises

--- src/GettersandSetters/Account.java
+++ src/GettersandSetters/Account.java
@@ -3,16 +3,28 @@ package GettersandSetters;
 public class Account {
     String name;
     int age;
-    public static void main(String [] args){
 
+//    [public static]= modifier, [void]=return type,
+//    [main]=method type, (String [] args)=[formal paramaters]
+    public static void main(String [] args){
+// This area Method body where the code goes
+        add( 2,3);
+        add(1.2f,3.4f);
+        add();
+        System.out.println(isEven(4));
+        isEven(5);
+//        System.out.println(add(2,3));
         Account a = new Account();
         a.setName("Pablo");
         a.setAge(34);
+        a.printDetails();
         System.out.println(a.getAge());
         System.out.println(a.getName());
-        a.printDetails();
+
+
 
     }
+//    getters and setters examples
     public void setName(String name){
         this.name = name;
     }
@@ -29,7 +41,39 @@ public class Account {
     }
 
     public void printDetails(){
+        System.out.println("this is getters and setters");
         System.out.println(name + "," + age);
     }
+
+//    method overloading examples
+    private static void add(){
+    int sum = 3 + 4;
+    System.out.println(sum);
+    }
+//    private static int add(int a, int b){
+//        int sum = a + b;
+//        return sum;
+//    }
+    private static void add(float a, float b){
+        float sum = a + b;
+        System.out.println(sum);
+    }
+
+
+
+
+
+//    constructors examples
+    private static String isEven(int num){
+        String str;
+//    private static void isEven(int num) {
+        if (4 % 2 == 0)
+            str = num + " is Even";
+//            System.out.println(num + " is even");
+        else
+            System.out.println(num + " is odd");
+        return str;
+    }
+
 
 }
---
+++ src/GettersandSetters/Dog.java
@@ -0,0 +1,15 @@
+package GettersandSetters;
+
+public class Dog{
+//    properties String variable
+    String breed;
+    int age;
+    String color;
+
+    void bark(){
+        int x;
+    }
+    void run(){
+        String x;
+    }
+}
